1703415006a12089506aa5972136676b71ba431a
Darguima <dsgdevbraga@gmail.com>
2022-04-13 09:37:10 +0100

    Spotify Credentials Page Interface

--- mobile/src/pages/LoginPage/pages/SpotifyCredentialsPage/index.tsx
+++ mobile/src/pages/LoginPage/pages/SpotifyCredentialsPage/index.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
-import { Text, View, StyleSheet, Dimensions } from 'react-native'
+import { Text, View, TextInput, StyleSheet, Dimensions, Linking } from 'react-native'
 
 const SpotifyCredentialsPage:React.FC = () => {
 	return (
 		<View style={styles.container}>
-			<Text></Text>
+
+			<View style={styles.infoTextContainer}>
+
+				<Text style={styles.infoTextTitle}>Spotify Credentials</Text>
+
+				<Text style={styles.infoText}>
+					How to get Spotify Developer Credentials:
+					{'\n\n'}
+					1. Access <Text style={{ color: '#00f' }} onPress={() => Linking.openURL('https://developer.spotify.com/dashboard/login')} >Spotify For Developers</Text>.
+					{'\n'}
+					2. Login with your Spotify Account.
+					{'\n'}
+					3. Create An App.
+					{'\n'}
+					4. Copy the credentials.
+					{'\n\n'}
+					Watch our <Text style={{ color: '#00f' }} onPress={() => Linking.openURL('https://github.com/Darguima/SpotHack')} >GitHub tutorial video</Text>.
+				</Text>
+
+			</View>
+
+			<View style={styles.inputArea}>
+
+				<View style={styles.inputContainer}>
+					<Text style={styles.inputInfoText}>Client Id:</Text>
+					<TextInput style={styles.input} />
+				</View>
+
+				<View style={styles.inputContainer}>
+					<Text style={styles.inputInfoText}>Client Secret:</Text>
+					<TextInput style={styles.input} />
+				</View>
+
+			</View>
+
 		</View>
 	)
 }
@@ -12,10 +46,50 @@ const SpotifyCredentialsPage:React.FC = () => {
 const styles = StyleSheet.create({
 	container: {
 		width: Dimensions.get('window').width,
-		height: Dimensions.get('window').height,
+		height: Dimensions.get('window').height
+	},
 
-		justifyContent: 'space-around',
+	infoTextContainer: {
+		width: '100%',
+		height: '40%',
+
+		justifyContent: 'space-between',
 		alignItems: 'center'
+	},
+
+	infoTextTitle: {
+		textAlign: 'center',
+		fontSize: 22,
+		fontWeight: 'bold'
+	},
+
+	infoText: {
+		width: '95%',
+		fontSize: 16
+	},
+
+	inputArea: {
+		justifyContent: 'space-evenly',
+		alignItems: 'center',
+
+		width: '100%',
+		height: '60%'
+	},
+
+	inputContainer: {
+		width: '80%'
+	},
+
+	inputInfoText: {
+		fontSize: 16,
+		marginBottom: 8
+	},
+
+	input: {
+		backgroundColor: '#fff',
+
+		borderRadius: 8,
+		borderWidth: 2
 	}
 })
 
